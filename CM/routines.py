dcb4298fa80c8cdb382afcb5216098489a7da577
bischrob <bischrob@gmail.com>
2025-02-11 08:27:55 -0700

    saving current state

--- CM/routines.py
+++ CM/routines.py
@@ -3,27 +3,65 @@
 # This is a module for automatic routines in CatMapper
 
 from neo4j import GraphDatabase
+from CM.utils import *
+from CM.email import *
+import pandas as pd
+import json
 
-def addLog(driver):
+def is_valid_json(json_string):
     try:
+        json.loads(json_string)
+        return True
+    except json.JSONDecodeError:
+        return False
+
+def validateJSON(database, property = 'parentContext', path ="/mnt/storage/app/tmp/invalid_json.xlsx" ):
+    try:
+
+        driver = getDriver(database)
+
+        query = f"match (c:CATEGORY)<-[r:USES]-(d:DATASET) where not r.{property} is null return d.CMID as datasetID, c.CMID as CMID, r.Key as Key, r.{property} as prop"
+
+        results = getQuery(query, driver)
+
+        results = pd.DataFrame(results)
+
+        results = pd.DataFrame.explode(results, "prop")
+
+        results["is_valid_json"] = results["prop"].apply(is_valid_json)
+
+        invalid = results[results["is_valid_json"] == False]
+
+        invalid.to_excel(path)
+
+        invalid = invalid.to_dict(orient="records")
+
+        return(invalid)
+
+    except Exception as e:
+        return "Unable to validate JSON properties: " + str(e)
+    
+
+def addLog(database):
+    try:
+        driver = getDriver(database)
         query = """
         match ()-[r:USES]-() where r.log is null set r.log = custom.makeLog([],"0","added log property")
         """
-        with driver.session() as session:
-                results = session.run(query)
-                driver.close()
+        getQuery(query, driver)
+
         query = """
         match (c) where c.log is null set c.log = [custom.makeLog([],"0","added log property")]
         """
-        with driver.session() as session:
-                results = session.run(query)
-                driver.close()
+        getQuery(query, driver)
+
         return "Completed"
     except Exception as e:
         return str(e)
     
-def checkDomains(data,driver):
+def checkDomains(data,database):
     try:
+        driver = getDriver(database)
         query = """
         match (n:CATEGORY)
         where size(labels(n)) = 1
@@ -35,14 +73,12 @@ def checkDomains(data,driver):
         optional match (n)<-[r:USES]-(d:DATASET) 
         return "MissingCATEGORY" as query,  n.CMID as CMID, n.CMName as CMName, r.label as label, d.CMID as datasetID
         """
-        with driver.session() as session:
-                results = session.run(query)           
-                result = [dict(record) for record in results]
-                if data is False:
-                     result = result
-                driver.close()
+        result = getQuery(query, driver)
         
-        return result
+        if data:
+            return result
+        else:
+            return "Completed"
          
     except Exception as e:
         return str(e)
@@ -50,3 +86,124 @@ def checkDomains(data,driver):
 
 # need to create function based off this
 # with "eventType" as prop match (a)-[r]->(b) where not r[prop] is null and r[prop] = [] call apoc.cypher.doIt("with r set r." + prop + " = NULL",{r:r}) yield value return count(*)
+
+def backup2CSV(database, mail = None):
+    try:
+        driver = getDriver(database)
+        query = """
+            with 'match (d:DATASET) unwind keys(d) as property return distinct id(d) as nodeID, property, d[property] as value' as query CALL apoc.export.csv.query(query, '/backups/datasetNodes.csv', {})
+            YIELD file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data
+            RETURN count(*);
+        """
+
+        result = getQuery(query, driver)
+        print(result)
+         
+        query2 = """
+            with 'match (n:CATEGORY)<-[r:USES]-(d:DATASET) unwind keys(r) as property return distinct id(r) as relID, n.CMID as CMID, n.CMName as CMName, d.CMName as dataset, d.CMID as datasetID, property, r[property] as value order by CMName' as query CALL apoc.export.csv.query(query, '/backups/USESties.csv', {})
+            YIELD file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data
+            RETURN count(*);
+            """
+        
+        result2 = getQuery(query2, driver)
+        print(result2)
+
+        if mail:
+            sendEmail(mail, subject = "Weekly CSV Backup", recipients = ["rjbischo@catmapper.org"], body = "Weekly CSV backup completed.", sender = os.getenv("mail_default"))
+
+        return "backup2CSV completed"
+        
+    except Exception as e:
+        return str(e)
+    
+
+
+def getBadCMID(database,mail = None):
+    try:
+         
+        driver = getDriver(database)
+        properties = getPropertiesMetadata(driver)
+        properties = pd.DataFrame(properties)
+        properties = properties[properties["relationship"].notna()]
+
+        results = []
+
+        for property in properties["property"]:
+            print("Checking CMIDs for property: ", property)
+
+            queryFix = """
+        MATCH (c:CATEGORY)<-[r:USES]-(d:DATASET)
+        WHERE not r.{property} is null and not apoc.meta.cypher.type(r.{property}) = "LIST OF STRING" set r.{property} = [r.{property}]
+        """
+
+            query = f"""
+            MATCH (c:CATEGORY)<-[r:USES]-(d:DATASET) 
+            with apoc.coll.toSet(apoc.coll.flatten(collect(distinct r.{property}),true)) as val
+            call {{with val match (c:CATEGORY) where c.CMID in val return collect(c.CMID) as val2}} 
+            with [i in val where not i in val2] as badlist
+            unwind badlist as bad
+            with bad
+            match (c:CATEGORY)<-[r:USES]-(d:DATASET) where bad in r.{property} 
+            return c.CMID as CMID, c.CMName as CMName, r.Key as Key, d.CMID as datasetID, d.CMName as dataset, '{property}' as propertyType, apoc.text.join(r.{property},"; ") as property, bad as badCMID
+            """
+            result = getQuery(query, driver)
+            results.append(result)
+
+        results = [pd.DataFrame(item) for item in results]
+
+        if len(results) > 0:
+            results = pd.concat(results)
+            query2 = """
+            MATCH (old:DELETED)-[:IS]->(c) 
+            where old.CMID in $cmids 
+            return old.CMID as badCMID, c.CMID as newCMID
+            """
+
+            deleted = getQuery(query2, driver, {"cmids": list(results["badCMID"].unique())})
+
+            deleted = pd.DataFrame(deleted)
+
+            results = results.merge(deleted, on = "badCMID", how = "left")
+
+            if mail is not None:
+                fp1 = "tmp/badCMIDs.xlsx"
+                results.to_excel(fp1, index = False)
+                sendEmail(mail, subject = "Bad CMIDs", recipients = ["rjbischo@catmapper.org"], body = "See attached", sender = os.getenv("mail_default"), attachments = [fp1])
+
+            return results.to_dict(orient="records")
+        else: 
+            print("No bad CMIDs found")
+
+    except Exception as e:
+        return str(e)
+        
+
+def getMultipleLabels(database):
+    try:
+        driver = getDriver(database)
+        query = """
+        match (d:DATASET)-[r:USES]->(c:CATEGORY) where apoc.meta.cypher.type(r.label) = "LIST OF STRING" and size(r.label) > 1 return c.CMID as CMID, c.CMName as CMName, d.CMID as datasetID, r.Key as Key, apoc.text.join(r.label, "; ") as label
+        """
+        results = getQuery(query, driver)
+        return results
+    except Exception as e:
+        return str(e)
+    
+def getBadJSON(database,mail = None):
+    try:        
+        fp1 = "tmp/invalid_json_geoCoords.xlsx"
+        results1 = validateJSON(database = database, property = 'geoCoords', path = fp1)
+        fp2 = "tmp/invalid_json_parentContext.xlsx"
+        results2 = validateJSON(database = database, property = 'parentContext', path = fp2)
+
+        if mail is not None:
+            if results1:
+                sendEmail(mail, subject = "Invalid geoCoords properties", recipients = ["rjbischo@catmapper.org"], body = "See attached", sender = os.getenv("mail_default"), attachments = [fp1])
+
+            if results2:
+                sendEmail(mail, subject = "Invalid parentContext properties", recipients = ["rjbischo@catmapper.org"], body = "See attached", sender = os.getenv("mail_default"), attachments = [fp2])
+
+        return {"geoCoords": len(results1), "parentContext": len(results2), "geoCoords": results1, "parentContext": results2}
+    except Exception as e:
+        result = str(e)
+        return result, 500  
--- test.py
+++ test.py
@@ -18,3 +18,136 @@ result = input_Nodes_Uses(dataset = df,
                             geocode=False,
                             batchSize=1000)
 
+
+from CM import *
+import pandas as pd
+updateType = "update"
+database = "SocioMap"
+propertyType = "USES"
+user = "1"
+links = [{'datasetID': "SD2177", 'CMID': "SM250784", 'Key': "OWC: FF38", 'url': "https://ehrafworldcultures.yale.edu/cultures/ff38"}]
+links = pd.DataFrame(links)
+if not updateType in ['overwrite','update']:
+            raise Exception("type must be update or overwrite.")
+
+driver = getDriver(database)
+
+if propertyType == "USES":
+    requiredCols = ["datasetID", "CMID", "Key"]
+elif propertyType == "DATASET":
+    requiredCols = ["CMID"]
+else:
+    raise Exception("Invalid propertyType")
+
+for required in requiredCols:
+    if required not in links.columns:
+        raise ValueError(f"Missing required column {required}")
+    
+vars = links.drop(columns=[col for col in requiredCols if col in links.columns]).columns.tolist()
+
+if updateType == "update":
+    links['log'] = links.apply(lambda row: f"{time.strftime('%Y-%m-%dT%H:%M:%SZ')} user {user}: updated properties {', '.join([str(var) for var in vars])}", axis=1)
+else:
+    links['log'] = links.apply(lambda row: f"{time.strftime('%Y-%m-%dT%H:%M:%SZ')} user {user}: overwrote properties {', '.join([str(var) for var in vars])}", axis=1)
+
+vars = links.drop(columns=[col for col in requiredCols if col in links.columns]).columns.tolist()
+
+query = """
+match (n:METADATA:PROPERTY) 
+return n.property as property, n.type as type, 
+n.relationship as relationship, n.description as description, 
+n.display as display, n.group as group, n.metaType as metaType, n.search as search, n.translation as translation
+"""
+
+metaTypes = getQuery(query, driver)
+metaTypeDict = {item['property']: item['metaType'] for item in metaTypes}
+
+keys = []
+for var in vars:
+    metaType = metaTypeDict.get(var)  # Get the metaType for the given property
+    if updateType == "overwrite" and var != 'log':
+        keys.append(f"r.{var} = custom.combinedProperties('',row.{var},'{metaType}')[0].prop")
+    else:
+        keys.append(f"r.{var} = custom.combinedProperties(r.{var},row.{var},'{metaType}')[0].prop")
+
+keys = ", ".join(keys)
+
+if propertyType == "USES":
+    q = f"""
+    UNWIND $rows AS row
+    MATCH (a:DATASET {{CMID: row.datasetID}})-[r:USES {{Key: row.Key}}]->(b:CATEGORY {{CMID: row.CMID}}) 
+    WITH row, r, b
+    SET {keys}, r.status = "update"
+    RETURN id(b) as nodeID, b.CMID as CMID, row.Key as Key, row.datasetID as datasetID, row.parent as parent, row.parentContext as parentContext
+    """
+else:
+    q = f"""
+    UNWIND $rows AS row
+    MATCH (r:DATASET {{CMID: row.CMID}})
+    SET {keys}, r.status = "update"
+    RETURN id(r) as nodeID, r.CMID as CMID
+    """
+
+links_dict = links.to_dict(orient = "records")
+
+result = getQuery(query = q, driver = driver, params = {"rows": links_dict})
+
+if 'geoCoords' in links.columns:
+    updateLog(f"log/{user}uploadProgress.txt", "Updating geo coordinates", write = 'a')
+    CMIDs = links['CMID'].unique()
+    correct_geojson(CMID = CMIDs, database = database)
+
+
+from CM import *
+# backup2CSV(database = "ArchaMap")
+
+database = "sociomap"
+driver = getDriver(database)
+properties = getPropertiesMetadata(driver)
+properties = pd.DataFrame(properties)
+properties = properties[properties["relationship"].notna()]
+
+results = []
+
+for property in properties["property"]:
+    print("Checking CMIDs for property: ", property)
+
+    queryFix = """
+MATCH (c:CATEGORY)<-[r:USES]-(d:DATASET)
+WHERE not r.{property} is null and not apoc.meta.cypher.type(r.{property}) = "LIST OF STRING" set r.{property} = [r.{property}]
+"""
+
+    query = f"""
+    MATCH (c:CATEGORY)<-[r:USES]-(d:DATASET) 
+    with apoc.coll.toSet(apoc.coll.flatten(collect(distinct r.{property}),true)) as val
+    call {{with val match (c:CATEGORY) where c.CMID in val return collect(c.CMID) as val2}} 
+    with [i in val where not i in val2] as badlist
+    unwind badlist as bad
+    with bad
+    match (c:CATEGORY)<-[r:USES]-(d:DATASET) where bad in r.{property} 
+    return c.CMID as CMID, c.CMName as CMName, r.Key as Key, d.CMID as datasetID, d.CMName as dataset, '{property}' as propertyType, apoc.text.join(r.{property},"; ") as property, bad as badCMID
+    """
+    result = getQuery(query, driver)
+    results.append(result)
+
+results = [pd.DataFrame(item) for item in results]
+
+if len(results) > 0:
+    results = pd.concat(results)
+    query2 = """
+    MATCH (old:DELETED)-[:IS]->(c) 
+    where old.CMID in $cmids 
+    return old.CMID as badCMID, c.CMID as newCMID
+    """
+
+    deleted = getQuery(query2, driver, {"cmids": list(results["badCMID"].unique())})
+
+    deleted = pd.DataFrame(deleted)
+
+    results = results.merge(deleted, on = "badCMID", how = "left")
+
+    results.to_excel("badCMID.xlsx", index = False)
+else: 
+    print("No bad CMIDs found")
+
+
