a87a0041de00997f0f567663d54a396fc7dcd281
bischrob <bischrob@gmail.com>
2025-06-05 09:13:29 -0700

    updated current status

--- CMroutes/getLogs.py
+++ CMroutes/getLogs.py
@@ -1,27 +1,28 @@
 from CM import getDriver, getQuery
 
+
 def getLogs(database, CMID):
     if not isinstance(CMID, str):
         return "Invalid CMID format. It should be a string."
     driver = getDriver(database)
     if not driver:
         return "Database connection failed."
-    
+
     query = """
     MATCH (a {CMID: $CMID})-[:HAS_LOG]->(log:LOG)
     return "node" as log_type, elementId(log) as ID, log.user as user, log.action as action, log.timestamp as timestamp
     order by timestamp desc
     UNION ALL
-    MATCH (:CATEGORY {CMID: $CMID})<-[r:USES]-(:DATASET)
+    MATCH (:CATEGORY {CMID: $CMID})<-[r:USES]-(d:DATASET)
     where not r.logID is null
-    with r.logID as logID
+    with r.logID as logID, r.Key as Key, d.CMID as datasetID
     match (log:LOG) where elementId(log) in logID
-    return "relationship" as log_type, elementId(log) as ID, log.user as user, log.action as action, log.timestamp as timestamp 
+    return "relationship: (Key) " + Key + " (datasetID) " + datasetID as log_type, elementId(log) as ID, log.user as user, log.action as action, log.timestamp as timestamp 
     order by timestamp desc
     """
     logs = getQuery(query=query, driver=driver, CMID=CMID)
 
     if isinstance(logs, dict):
         return logs
-    else: 
+    else:
         return {"logs": logs}
