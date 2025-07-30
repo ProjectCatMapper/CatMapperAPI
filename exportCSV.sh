56da5282ef25eabb3a70589f1ba839084ec5d27c
bischrob <bischrob@gmail.com>
2025-07-30 15:54:55 -0700

    saving current state

--- exportCSV.sh
+++ exportCSV.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+# !/bin/bash
 
-# api_url="https://catmapper.org/api/routines/backup2CSV/ArchaMap"
+api_url="https://catmapper.org/api/routines/backup2CSV/ArchaMap"
 
-# response=$(curl -ks "Content-Type: application/json" "$api_url")
+response=$(curl -ks "Content-Type: application/json" "$api_url")
 
-# echo "API Response for backup CSV in ArchaMap:"
-# echo "$response"
+echo "API Response for backup CSV in ArchaMap:"
+echo "$response"
 
-# api_url="https://catmapper.org/api/routines/backup2CSV/SocioMap"
+api_url="https://catmapper.org/api/routines/backup2CSV/SocioMap"
 
-# response=$(curl -ks "Content-Type: application/json" "$api_url")
+response=$(curl -ks "Content-Type: application/json" "$api_url")
 
-# echo "API Response for backup CSV in ArchaMap:"
-# echo "$response"
+echo "API Response for backup CSV in ArchaMap:"
+echo "$response"
 
 chmod -R 777 /mnt/storage/app/db/sociomap1/backups;
 chmod -R 777 /mnt/storage/app/db/archamap1/backups;
