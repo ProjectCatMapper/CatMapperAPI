320ea3db80863dd3b9b19d39559d72f2d4e02f7d
bischrob <bischrob@gmail.com>
2025-07-30 15:54:55 -0700

    saving current state

--- test.py
+++ test.py
@@ -2,4 +2,20 @@ from CM import *
 
 database = 'sociomap'
 driver = getDriver(database)
-CMID = None
+CMID = ["AM1","AM2","AM3","AM4","AM5","AM6","AM7","AM8","AM9","AM10"]
+domain = "CATEGORY"
+
+if domain == "CATEGORY":
+    query = """
+    unwind $CMID as cmid
+    match (c:CATEGORY {cmid: cmid})<-[r:USES]-(d:DATASET)
+    return c.CMID as CMID, c.CMName as CMName, 
+    """
+elif domain == "DATASET":
+    query = """
+    unwind $CMID as cmid
+    match (d:DATASET {cmid: cmid})
+    return d.cmid, d.name, d.description
+    """
+else: 
+    raise ValueError("Invalid domain specified")
