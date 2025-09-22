e3965ad8581c32cdfa270e3addedbe03e95712fc
Robert Bischoff <rbischoff@asu.edu>
2025-09-22 12:15:03 -0700

    saving current state

--- pullBackupsFromS3.sh
+++ pullBackupsFromS3.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 chmod -R 777 /mnt/storage/app/db/sociomap1/backups;
+chmod -R 777 /mnt/storage/app/db/archamap1/backups;
+chmod -R 777 /mnt/storage/app/GISdb/backups;
+chmod -R 777 /mnt/storage/app/userdb/backups;
 
 rm -rf /mnt/storage/app/db/sociomap1/data;
+rm -rf /mnt/storage/app/db/archamap1/data;
+rm -rf /mnt/storage/app/GISdb/data;
+rm -rf /mnt/storage/app/userdb/data;
 
-sudo -u ubuntu aws s3 cp s3://catmapper/backups/sociomap1/neo4j-backup.tar.zst /mnt/storage/app/db/sociomap1/backups/;
+sudo -u rjbischo aws s3 cp s3://catmapper/backups/sociomap1/neo4j-backup.tar.zst /mnt/storage/app/db/sociomap1/backups/;
 
+sudo -u rjbischo aws s3 cp s3://catmapper/backups/archamap1/neo4j-backup.tar.zst /mnt/storage/app/db/archamap1/backups/;
+
+sudo -u rjbischo aws s3 cp s3://catmapper/backups/gisdb/neo4j-backup.tar.zst /mnt/storage/app/GISdb/backups/;
+
+sudo -u rjbischo aws s3 cp s3://catmapper/backups/userdb/neo4j-backup.tar.zst /mnt/storage/app/userdb/backups/;
