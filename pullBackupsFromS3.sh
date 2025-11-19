574d9994a43bcfb572850a4f790361c07cc1623a
Robert Bischoff <rbischoff@asu.edu>
2025-11-19 09:26:21 -0700

    changed config

--- pullBackupsFromS3.sh
+++ pullBackupsFromS3.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+docker stop sociomap1 archamap1 gisdb userdb
+
 chmod -R 777 /mnt/storage/app/db/sociomap1/backups;
 chmod -R 777 /mnt/storage/app/db/archamap1/backups;
-chmod -R 777 /mnt/storage/app/GISdb/backups;
-chmod -R 777 /mnt/storage/app/userdb/backups;
+chmod -R 777 /mnt/storage/app/db/gisdb/backups;
+chmod -R 777 /mnt/storage/app/db/userdb/backups;
 
 rm -rf /mnt/storage/app/db/sociomap1/data;
 rm -rf /mnt/storage/app/db/archamap1/data;
-rm -rf /mnt/storage/app/GISdb/data;
-rm -rf /mnt/storage/app/userdb/data;
+rm -rf /mnt/storage/app/db/gisdb/data;
+rm -rf /mnt/storage/app/db/userdb/data;
 
 sudo -u rjbischo aws s3 cp s3://catmapper/backups/sociomap1/neo4j-backup.tar.zst /mnt/storage/app/db/sociomap1/backups/;
 
 sudo -u rjbischo aws s3 cp s3://catmapper/backups/archamap1/neo4j-backup.tar.zst /mnt/storage/app/db/archamap1/backups/;
 
-sudo -u rjbischo aws s3 cp s3://catmapper/backups/gisdb/neo4j-backup.tar.zst /mnt/storage/app/GISdb/backups/;
+sudo -u rjbischo aws s3 cp s3://catmapper/backups/gisdb/neo4j-backup.tar.zst /mnt/storage/app/db/gisdb/backups/;
 
-sudo -u rjbischo aws s3 cp s3://catmapper/backups/userdb/neo4j-backup.tar.zst /mnt/storage/app/userdb/backups/;
+sudo -u rjbischo aws s3 cp s3://catmapper/backups/userdb/neo4j-backup.tar.zst /mnt/storage/app/db/userdb/backups/;
+
+docker start sociomap1 archamap1 gisdb userdb
