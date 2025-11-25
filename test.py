f1b85d971e3dfb628805da10552fc9020a7301c4
Robert Bischoff <rbischoff@asu.edu>
2025-11-25 16:51:37 -0700

    fixed problems with pull request

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-from CM import *
-driver = getDriver("sociomap")
-re.compile(r"^\s*[^=&&]+?\s*==\s*[^=&&]+?(?:\s*&&\s*[^=&&]+?\s*==\s*[^=&&]+?)*\s*$")
+# from CM import *
+# driver = getDriver("sociomap")
+# re.compile(r"^\s*[^=&&]+?\s*==\s*[^=&&]+?(?:\s*&&\s*[^=&&]+?\s*==\s*[^=&&]+?)*\s*$")
 
-keys = getQuery*("MATCH (c:CATEGORY)<-[r:USES]-(d:DATASET)")
+# keys = getQuery*("MATCH (c:CATEGORY)<-[r:USES]-(d:DATASET)")
