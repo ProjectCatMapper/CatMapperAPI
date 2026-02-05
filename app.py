584f030982f8f19bd982deeaddc058f9dd4355fd
Robert Bischoff <rbischoff@asu.edu>
2026-02-04 18:44:10 -0700

    added documentation

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-"""First layer of contact between the frontend and backend, some functions diverge from here to other files but the rest are computed here."""
-
+"""
+app.py
+The main application file for the CatMapperAPI Flask application.
+endpoints are registered here and imported from the CMroutes directory.
+Use `grep -R "endpoint" CMroutes/` to find specific endpoints.
+e.g., `grep -R "/uploadInputNodes" CMroutes/` to find the /uploadInputNodes endpoint.
+Specific functions called by endpoints can be found in the CM directory.
+This directory can also be searched using `grep -R "function_name" CM/`.
+"""
 import atexit
 from CM import closeAllDrivers
 from CMroutes import *
 
 app = create_app()
-app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # 50 MB
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # 50 MB for api calls
 
 # routes
 app.register_blueprint(root_bp)  
@@ -21,7 +28,8 @@ app.register_blueprint(routine_bp)
 app.register_blueprint(download_bp)  
 app.register_blueprint(homepage_bp)  
 
-atexit.register(closeAllDrivers)
+atexit.register(closeAllDrivers) # closes all active web drivers on exit
 
+# run the app from pythong (development mode)
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
